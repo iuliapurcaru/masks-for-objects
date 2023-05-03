c87e5475ba94bcc58566ef161f5c053c9e89a503
iuliapurcaru <74298954+iuliapurcaru@users.noreply.github.com>
2023-05-03 23:32:20 +0300

    Added crop

--- main.py
+++ main.py
@@ -1,30 +1,25 @@
 from PIL import Image
 
-fp = open("image-compressed-2.txt")
-for i, line in enumerate(fp):
-    if i == 1:
-        line = fp.readline()
-        break
-fp.close()
 
-array = line.split()
-array = list(map(float, array))
+def crop_image(index):
+    with open("resurse/image-" + str(index) + ".txt") as fp:
+        for i, line in enumerate(fp):
+            array = line.split()
+            if array[0] == '72':
+                break
 
-x_center = array[1] * 100
-y_center = array[2] * 100
-width = array[3] * 100
-height = array[4] * 100
+    array = list(map(float, array))
+    class_, x_center, y_center, width, height = array
 
-left = x_center - width/2 - height/2
-top = y_center - width/2 - height/2
-right = x_center + width/2 + height/2
-bottom = y_center + width/2 + height/2
+    img = Image.open("resurse/image-" + str(index) + ".jpeg")
+    img_height, img_width = img.size
+    left = (x_center - width/2) * img_width
+    top = (y_center - height/2) * img_height
+    right = (x_center + width/2) * img_width
+    bottom = (y_center + height/2) * img_height
 
-left = 54
-top = 150
-right = 180
-bottom = 430
+    img = img.crop((left, top, right, bottom))
+    img.save("resurse/image-crop-" + str(index) + ".jpeg")
 
-img = Image.open(r"imagini/image-2.jpeg")
-img_res = img.crop((left, top, right, bottom))
-img_res.show()
+
+crop_image(2)
